56c6720ba5febe2588d625a69138fb360562b5d8
unknown <howellmr98@gmail.com>
2022-01-11 16:29:30 -0500

    starting stretch

--- index.js
+++ index.js
@@ -226,12 +226,11 @@ Using the annoyingSong function below do the following:
 
 
 function annoyingSong(number){
-      while(number > 0){
-        return `${number} bottles of soda on the wall, ${number} bottles of soda, take one down pass it around ${number - 1} bottles of soda on the wall`;
-      }
-      number - 1;
+     for(let i = number; i > 0; i--){
+       return `${number} bottles of soda on the wall, ${number} bottles of soda, take one down pass it around ${number - 1} bottles of soda on the wall`
+     }
 }
-
+console.log(annoyingSong(3))
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
@@ -248,8 +247,18 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(/*Your Code here */){
-/*Your Code here */
+function grade(score){
+  if(score > 90){
+    return 'you got an A';
+  }else if(score > 80){
+    return 'you got a B';
+  }else if(score > 70){
+    return 'you got a C';
+  }else if(score > 60){
+    return 'you got a D';
+  }else{
+    return 'you got an F';
+  }
 }
 
 
